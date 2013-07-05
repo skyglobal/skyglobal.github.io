0677045b2750b1552932fa0f7101f3d16c98f36a
skyhaikuteam <skyhaikuteam@gmail.com>
2013-07-05 08:50:46 +0100

    ET: started to make the site look a little prettier

---
+++ javascript/app.js
@@ -0,0 +1,29 @@
+// $(function() {
+//     init();
+// });
+
+// var init = function(){
+// 	setupEvents();
+// };
+
+// var setupEvents = function(){
+// 	$(".projects").on("click", function(evt){
+// 		evt.preventDefault();
+// 		if(evt.target !== null){
+// 			if($(evt.target).is("#octocat")){
+// 				//do something
+// 				return;
+// 			}
+// 			if($(evt.target).is("#toolkit")){
+// 				//do something
+// 				return;
+// 			}
+// 			if($(evt.target).is("#bonoculars")){
+// 				//do something
+// 				return;
+// 			}
+// 		}
+// 	});
+// };
+
+// // var 
